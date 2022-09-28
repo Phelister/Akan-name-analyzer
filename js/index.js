a68b8adcbc0a5b75202a9a97af63719a5257dbca
pbogonko <pbogonko@safaricom.co.ke>
2022-09-28 12:39:26 +0300

    updated the intro section

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const MALE_NAMES=[];
+const FEMALE_NAMES=[];
+const DAYS_OF_THE_WEEK=[];
+// You can use a form or prompt form you use jquery
+
+//get user input, user's birthday and gender
+//m or f
+
+//calculate day of birth:
+
+//get users name
+//
+var index_dayof week=
+
+function getAkhanNames(day,month, year,gender){
+    
+}
