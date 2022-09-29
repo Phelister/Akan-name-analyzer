12fd2b72d52ea0c8efe508533542398111dc5520
pbogonko <pbogonko@safaricom.co.ke>
2022-09-29 10:38:55 +0300

    added switch section on javaScript

--- js/index.js
+++ js/index.js
@@ -14,8 +14,72 @@ var dayOfWeek;
 
 function getAkhanNames(date, gender){
     if(date== null ||)
+    
     date.toString();
+    gender.toString.toLowerCase;
+    gender.toLowerCase();
     dayOfWeek=(((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7;
+    switch(dayOfWeek){
+        case 0:
+            if(gender=== "male"){
+                akhanName=MALE_NAMES[0];
+            }
+            else{
+                akhanName=FEMALE_NAMES[0];
+            }
+            break;
+        case 1:
+            if(gender==="male"){
+                akhanName=MALE_NAMES[1];
+            }
+            else{
+                akhanName=FEMALE_NAMES[1];
+            }
+            break;
+        case 2:
+            if(gender==="male"){
+                akhanName=MALE_NAMES[2];
+            }
+            else{
+                akhanName=FEMALE_NAMES[2];
+            }
+            break;
+        case 3:
+            if(gender==="male"){
+                akhanName=MALE_NAMES[3];
+            }
+            else{
+                akhanName=FEMALE_NAMES[3];
+            }
+            break;
+        case 4:
+            if(gender==="male"){
+                akhanName=MALE_NAMES[4];
+            }
+            else{
+                akhanName=FEMALE_NAMES[4];
+            }
+            break;
+        case 5:
+            if(gender==="male"){
+                akhanName=MALE_NAMES[5];
+            }
+            else{
+                akhanName=FEMALE_NAMES[5];
+            }
+            break;
+
+        case 6:
+            if(gender==="male"){
+                akhanName=MALE_NAMES[6];
+            }
+            else{
+                akhanName=FEMALE_NAMES[6];
+            }
+            break;
+        
+    }
+    return akhanName;
 }
 
 
